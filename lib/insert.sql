05e44a4b5bf9ff97ec32d414d848911861aa5dd6
Jonathan Hong <jonhong777@gmail.com>
2018-08-03 16:29:34 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 3, "M", "Brown", "Shy", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 6, "F", "Brown", "Aggressive", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 3, "M", "Brown", "Shy", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 3, "M", "Brown", "Shy", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 3, "M", "Brown", "Shy", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 3, "M", "Brown", "Shy", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 10, "M", "Brown", "Calm", false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 2, "M", "Brown", "Super Aggressive", false);
